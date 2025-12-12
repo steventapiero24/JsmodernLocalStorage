41495cbef2e4e9f3902d7e2bb7b7b50a459de9ca
steventapiero24 <steventapiero24@gmail.com>
2025-12-12 09:21:18 +0100

    First commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// Variables
+const Formulario = document.querySelector('#formulario');
+const ListaTweets = document.querySelector('#lista-tweets');
+let tweets = [];
+
+//Event listeners
+
+eventListeners();
+function eventListeners () {
+
+}
+
+
+// Funciones
+
+localStorage.setItem("nombre", "Juan");
+
