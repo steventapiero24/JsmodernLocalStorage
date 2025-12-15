cb9e952bd5eaac7a94102e8bdc288e0e45e2d3b8
steventapiero24 <steventapiero24@gmail.com>
2025-12-15 09:45:21 +0100

    agregar a la lista

--- js/app.js
+++ js/app.js
@@ -21,8 +21,20 @@ function addTweet (e) {
         mostrarError('No puede ir vacio');
 
         return; // no se ejecuta mas el codigo
+
     }
 
+    // identificador uinico 
+    const tweetObj = {
+        id : Date.now(),
+        tweet : tweet  // se podria pasar tambien solo tweet
+    }
+
+    // Añadir arreglo tweets
+    tweets=[...tweets, tweetObj];
+    console.log(tweets);
+
+    crearHtml();
 }
 
 function mostrarError (error) {
@@ -39,3 +51,32 @@ function mostrarError (error) {
         mensajeError.remove();
     },3000)
 }
+
+// crear Html
+
+function crearHtml () {
+
+    limpiarHtml();
+    
+    if ( tweets.length > 0 ) {
+        tweets.forEach ( tweet => {
+            const li = document.createElement('li');
+
+            // Añadir texto
+            li.innerText = tweet.tweet;
+
+            ListaTweets.appendChild(li);
+        })  
+
+        // agregarlo a la lista de tweets
+
+    }
+}
+
+// limpiar Html
+
+function limpiarHtml () {
+    while (ListaTweets.firstChild) {
+        ListaTweets.removeChild(ListaTweets.firstChild);
+    }   
+}   
